8b9723413f070dab230001953b3f3cf161684921
Ojosh <111340682+ojoscreate@users.noreply.github.com>
2026-09-05 07:46:39 +0100

    update

--- C-Sharp/start/obj/Release/net9.0/start.AssemblyInfo.cs
+++ C-Sharp/start/obj/Release/net9.0/start.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("start")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76b87b337fc44048d846f198d7b524c056b09fc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afb1f9176a926015474064bb8e55d45a1d540074")]
 [assembly: System.Reflection.AssemblyProductAttribute("start")]
 [assembly: System.Reflection.AssemblyTitleAttribute("start")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
