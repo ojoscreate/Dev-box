e7b7e4657e89a3f6c50f1bc9c6e31b9ece88bb90
Ojosh <111340682+ojoscreate@users.noreply.github.com>
2026-09-09 17:19:30 +0100

    update

--- udemy_javascript/BackEnd/BackEnd_Express.js/index.js
+++ udemy_javascript/BackEnd/BackEnd_Express.js/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 const app = express();
-const PORT = 35279;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World josh is ha a jasg!");
+  res.send("Hello World");
 });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
